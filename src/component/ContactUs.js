c099dd37f4e261ac3ee2308a0d779e41db2103d7
Kaushik440 <kaushikpandey440@gmail.com>
2024-05-10 23:06:25 +0530

    contactus backend

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -2,21 +2,38 @@ import React from 'react';
 import './ContactUs.css';
 
 function ContactUs() {
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    fetch('http://localhost/React-php/submitcontactform.php', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.text())
+    .then(data => {
+      console.log(data); // You can handle the response here
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+
   return (
-   <>
-   <div class="containercontactform">
-      <form id="contactForm" onsubmit="submitForm(event)">
+    <div className="containercontactform">
+      <form id="contactForm" onSubmit={submitForm}>
         <h2><u>Contact Us</u></h2>
-        <label for="name">Name:</label>
+        <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" required />
 
-        <label for="email">Email:</label>
+        <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" required />
 
-        <label for="contactNumber">Contact Number:</label>
+        <label htmlFor="contactNumber">Contact Number:</label>
         <input type="text" id="contactNumber" name="contactNumber" required />
 
-        <label for="message">Message:</label>
+        <label htmlFor="message">Message:</label>
         <textarea id="message" name="message" required></textarea>
 
         <button type="submit">Submit</button>
@@ -24,11 +41,6 @@ function ContactUs() {
 
       <div id="response"></div>
     </div>
-   
-
-
-   
-   </>
   );
 }
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -7,118 +7,115 @@ import GKP from "./images/gkp.jpg";
 import Lucknow from "./images/Lucknow.jpg";
 import Varanasi from "./images/varanasi.jpg"
 import './Home.css';
+
 function Home() {
   return (
-    <>
-
-      <div className="container1 justify-content-center">
-      <h2 className="homeExpc" mt-0>Explore City</h2>
-        <div id="carouselExampleCaptions" class="carousel slide " >
-          <div class="carousel-indicators" >
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active ">
-              <img
-                src={GKP}
-                class="d-block w-100 custom-carousel-image "
-                alt="..."
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5 style={{ color:'Blue' }}>Gorakhpur, Uttar Pradesh</h5>
-                <p>
-                  Some representative placeholder content for the first slide.
-                </p>
-              </div>
-            </div>
-            <div class="carousel-item ">
-              <img
-               src={Lucknow}
-                class="d-block w-100 custom-carousel-image"
-                alt="Lucknow"
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5 style={{ color:'Blue' }}>Lucknow,Uttar Pradesh</h5>
-                <p>
-                  Some representative placeholder content for the second slide.
-                </p>
-              </div>
-            </div>
-            <div class="carousel-item" >
-              <img
-                src={Varanasi}
-                class="d-block w-100 custom-carousel-image"
-                alt="..."
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5 style={{ color:'Blue' }}>Varanasi,Uttar Pradesh</h5>
-                <p>
-                  Some representative placeholder content for the third slide.
-                </p>
-              </div>
-            </div>
-          </div>
+    <div className="container1 justify-content-center">
+      <h2 className="homeExpc mt-0">Explore City</h2>
+      <div id="carouselExampleCaptions" className="carousel slide" >
+        <div className="carousel-indicators" >
           <button
-            class="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
           <button
-            class="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
         </div>
-       
-        <h3 className="ourcollection">Our Collections</h3>
-        <div className="container3">
-          
-          <div className="room">
-            <img src={BHK1} alt="Single Room" />
-            <p>1BHK</p>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={GKP}
+              className="d-block w-100 custom-carousel-image"
+              alt="Gorakhpur"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5 style={{ color:'Blue' }}>Gorakhpur, Uttar Pradesh</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
           </div>
-          <div className="room">
-            <img src={BHK2} alt="Double Room"  />
-            <p>2BHK</p>
+          <div className="carousel-item">
+            <img
+              src={Lucknow}
+              className="d-block w-100 custom-carousel-image"
+              alt="Lucknow"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5 style={{ color:'Blue' }}>Lucknow, Uttar Pradesh</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
           </div>
-          <div className="room">
-            <img src={BHK3} alt="Three Room" />
-            <p>3BHK</p>
+          <div className="carousel-item">
+            <img
+              src={Varanasi}
+              className="d-block w-100 custom-carousel-image"
+              alt="Varanasi"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5 style={{ color:'Blue' }}>Varanasi, Uttar Pradesh</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
           </div>
-          <div className="room">
-            <img src={Homeimg} alt="Home" />
-            <p>Home</p>
-          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <h3 className="ourcollection">Our Collections</h3>
+      <div className="container3">
+        <div className="room">
+          <img src={BHK1} alt="Single Room" />
+          <p>1BHK</p>
+        </div>
+        <div className="room">
+          <img src={BHK2} alt="Double Room" />
+          <p>2BHK</p>
+        </div>
+        <div className="room">
+          <img src={BHK3} alt="Three Room" />
+          <p>3BHK</p>
+        </div>
+        <div className="room">
+          <img src={Homeimg} alt="Home" />
+          <p>Home</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
