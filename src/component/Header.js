09f71e487db911c87bacb8bfb4ca347fa2f3773b
Kaushik440 <kaushikpandey440@gmail.com>
2024-05-13 15:21:25 +0530

    .

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import './Header.css';
-import SearchResults from './SearchResults';
+import "./Header.css";
+import SearchResults from "./SearchResults";
 
 function Header() {
   const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
@@ -10,15 +10,18 @@ function Header() {
   const [searchClicked, setSearchClicked] = useState(false);
 
   const handleSearch = async () => {
-    const city = document.getElementById('city').value;
-    const type = document.getElementById('type').value;
+    const city = document.getElementById("city").value;
+    const type = document.getElementById("type").value;
 
-    const response = await fetch(`http://localhost/React-php/search1.php?city=${city}&type=${type}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      `http://localhost/React-php/search1.php?city=${city}&type=${type}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     const data = await response.json();
     setSearchResults(data);
@@ -27,61 +30,77 @@ function Header() {
 
   return (
     <>
-    <header>
-      <div className="login">
-        {isAuthenticated && user && (
-          <div>
-            <p className='username'>{user.name}</p>
-          </div>
-        )}
-        {isAuthenticated ? (
-          <div>
-            <button className='Buttonlogin' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-              Log Out
-            </button>
-          </div>
-        ) : (
-          <div>
-            <button className='Buttonlogin' onClick={() => loginWithRedirect()}>Log In</button>
-          </div>
-        )}
-      </div>
-      
-      <nav className='navbar'>
-        <Link to="/" className="active" class="navclass">Home</Link>
-        <Link to="/about" className="navclass">About</Link>
-        <Link to="/contact" className="navclass">Contact</Link>
-        
-        <div className="search-container">
-          <label htmlFor="city">City:</label>
-          <select id="city" name="city">
-            <option value="">-- Select City --</option>
-            <option value="Gorakhpur">Gorakhpur</option>
-            <option value="Deoria">Deoria</option>
-            <option value="Basti">Basti</option>
-            <option value="Lucknow">Lucknow</option>
-            <option value="Delhi">Delhi</option>
-            {/* Add more cities as needed */}
-          </select>
-          <label htmlFor="type">Room Type:</label>
-          <select id="type" name="type">
-            <option value="">-- Select RoomType --</option>
-            <option value="1bhk">1BHK</option>
-            <option value="2bhk">2BHK</option>
-            <option value="1bhk">3BHK</option>
-            <option value="home">HOME</option>
-          </select>
-          <button onClick={handleSearch}>Search</button>
-          <Link className='searchhide' to="/0">...</Link>
+      <header>
+        <div className="login">
+          {isAuthenticated && user && (
+            <div>
+              <p className="username">{user.name}</p>
+            </div>
+          )}
+          {isAuthenticated ? (
+            <div>
+              <button
+                className="Buttonlogin"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button
+                className="Buttonlogin"
+                onClick={() => loginWithRedirect()}
+              >
+                Log In
+              </button>
+            </div>
+          )}
         </div>
-      </nav>
 
-      
-    </header>
-    <div className='SearchResultContainer'>
-    {/* Render search results component if search button is clicked */}
-    {searchClicked && <SearchResults results={searchResults} />}
-    </div>
+        <nav className="navbar">
+          <Link to="/" className="active" class="navclass">
+            Home
+          </Link>
+          <Link to="/about" className="navclass">
+            About
+          </Link>
+          <Link to="/contact" className="navclass">
+            Contact
+          </Link>
+
+          <div className="search-container">
+            <label htmlFor="city">City:</label>
+            <select id="city" name="city">
+              <option value="">-- Select City --</option>
+              <option value="Gorakhpur">Gorakhpur</option>
+              <option value="Deoria">Deoria</option>
+              <option value="Basti">Basti</option>
+              <option value="Lucknow">Lucknow</option>
+              <option value="Delhi">Delhi</option>
+              {/* Add more cities as needed */}
+            </select>
+            <label htmlFor="type">Room Type:</label>
+            <select id="type" name="type">
+              <option value="">-- Select RoomType --</option>
+              <option value="1bhk">1BHK</option>
+              <option value="2bhk">2BHK</option>
+              <option value="1bhk">3BHK</option>
+              <option value="home">HOME</option>
+            </select>
+            <button onClick={handleSearch}>Search</button>
+            <Link className="searchhide" to="/0">
+              ...
+            </Link>
+          </div>
+        </nav>
+      </header>
+      <div className="SearchResultContainer">
+        {/* Render search results component if search button is clicked */}
+        {searchClicked && <SearchResults results={searchResults} />}
+      </div>
     </>
   );
 }
--- src/component/SearchResults.js
+++ src/component/SearchResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './SearchResult.css';
+import "./SearchResult.css";
 
 function SearchResults({ results }) {
   return (
@@ -10,30 +10,62 @@ function SearchResults({ results }) {
             <div className="row g-0">
               <div className="col-md-4">
                 <div className="images-container">
-                  <div id={`carouselExampleAutoplaying-${index}`} className="carousel slide" data-bs-ride="carousel">
+                  <div
+                    id={`carouselExampleAutoplaying-${index}`}
+                    className="carousel slide"
+                    data-bs-ride="carousel"
+                  >
                     <div className="carousel-inner">
                       {result.image1_url && (
                         <div className="carousel-item active">
-                          <img src={`http://localhost/React-php/${result.image1_url}`} className="d-block w-100" alt={`City ${result.city}`} />
+                          <img
+                            src={`http://localhost/React-php/${result.image1_url}`}
+                            className="d-block w-100"
+                            alt={`City ${result.city}`}
+                          />
                         </div>
                       )}
                       {result.image2_url && (
                         <div className="carousel-item">
-                          <img src={`http://localhost/React-php/${result.image2_url}`} className="d-block w-100" alt={`City ${result.city}`} />
+                          <img
+                            src={`http://localhost/React-php/${result.image2_url}`}
+                            className="d-block w-100"
+                            alt={`City ${result.city}`}
+                          />
                         </div>
                       )}
                       {result.image3_url && (
                         <div className="carousel-item">
-                          <img src={`http://localhost/React-php/${result.image3_url}`} className="d-block w-100" alt={`City ${result.city}`} />
+                          <img
+                            src={`http://localhost/React-php/${result.image3_url}`}
+                            className="d-block w-100"
+                            alt={`City ${result.city}`}
+                          />
                         </div>
                       )}
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target={`#carouselExampleAutoplaying-${index}`} data-bs-slide="prev">
-                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <button
+                      className="carousel-control-prev"
+                      type="button"
+                      data-bs-target={`#carouselExampleAutoplaying-${index}`}
+                      data-bs-slide="prev"
+                    >
+                      <span
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
+                      ></span>
                       <span className="visually-hidden">Previous</span>
                     </button>
-                    <button className="carousel-control-next" type="button" data-bs-target={`#carouselExampleAutoplaying-${index}`} data-bs-slide="next">
-                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <button
+                      className="carousel-control-next"
+                      type="button"
+                      data-bs-target={`#carouselExampleAutoplaying-${index}`}
+                      data-bs-slide="next"
+                    >
+                      <span
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
+                      ></span>
                       <span className="visually-hidden">Next</span>
                     </button>
                   </div>
@@ -41,17 +73,30 @@ function SearchResults({ results }) {
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                
                   <h3 class="card-title">House Name : {result.house_name}</h3>
-                  <p class="card-text"></p>
-                  <p class="card-text"><pre>Address: {result.address}    City: {result.city} </pre></p>
-                  <p class="card-text"><pre>Price: {result.price}                         Room Type: {result.type} </pre></p>     
-                  <p class="card-text"><pre>Total Area: {result.carpet_area}                   Total Floor: {result.total_floor}</pre></p>     
-                  <p class="card-text"><pre>Floor Number: {result.floor_number}                Furnishing: {result.furnishing}</pre></p> 
-                  <p class="card-texto">Owner Name: {result.owner_name}</p>    
-                  <p class="card-text"><pre>Email address: {result.email} </pre></p>
-                  <p class="card-text">Contact Number: <a href="tel:+91{result.contact_number}">+91{result.contact_number}</a>    WhatsApp Number: 
-    <a href="https://api.whatsapp.com/send?phone=91{result.contact_number}&text=Hello,%20I%20would%20like%20to%20chat" target="_blank">WhatsApp Web Chat</a>
+                  <p class="card-text">
+                    <pre>  Address: {result.address} </pre>
+                  </p>
+                  <p class="card-text">
+                  City: {result.city} </p>
+                  <p class="card-text">
+                    <pre> Price: {result.price}   Room Type: {result.type} </pre>
+                  </p>
+                  <p class="card-text">
+                    <pre>
+                      Total Area: {result.carpet_area}   Total Floor: {result.total_floor}</pre>
+                  </p>
+                  <p class="card-text">
+                    <pre> Floor Number: {result.floor_number}   Furnishing:{result.furnishing} </pre>
+                  </p>
+                  <p class="card-texto">Owner Name: {result.owner_name}</p>
+                  <p class="card-text"> <pre>Email address: {result.email} </pre>
+                  </p> 
+                  <p class="card-text">
+                    Contact Number:{" "}
+                    <a href="tel:+91{result.contact_number}">
+                      +91{result.contact_number}
+                    </a>
                   </p>
                 </div>
               </div>
